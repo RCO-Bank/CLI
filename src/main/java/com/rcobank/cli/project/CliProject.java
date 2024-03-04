a9f9976732f9eeae6f602f5c5fed876e411bdab0
RCOliveira98 <rcoliveira.ti@gmail.com>
2024-03-04 15:45:07 -0300

    first commit

---
+++ src/main/java/com/rcobank/cli/project/CliProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rcobank.cli.project;
+
+/**
+ *
+ * @author dev_rco
+ */
+public class CliProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
